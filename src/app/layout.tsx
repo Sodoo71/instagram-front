99aceee0bb705f494493717fc1fa94483d8462aa
25HP6983 25HP6983 <25HP6983@G301-CC0211.local>
2025-09-26 18:27:13 +0800

    backend holbson

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "@/components/ui/sonner";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,12 +23,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster/>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,9 @@
-// page.tsx
+
 "use client";
 import { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+  
 
-  return (
-    <div className="w-xl min-h-screen mx-auto border-r-border border-l-border border">
-      <Navbar />
-      <div className="flex flex-col p-4 gap-4">
-        <Card>
-          <CardHeader>
-            <h1 className="text-2xl font-bold">Card title</h1>
-          </CardHeader>
-          <CardContent>
-            <img src="https://placehold.co/600x400/EEE/31343C" className="w-full h-full mb-4" alt="" />
-            <Button>Read more...</Button>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <h1 className="text-2xl font-bold">Card title</h1>
-          </CardHeader>
-          <CardContent>
-            <img src="https://placehold.co/600x400/EEE/31343C" className="w-full h-full mb-4" alt="" />
-            <Button>Read more...</Button>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <h1 className="text-2xl font-bold">Card title</h1>
-          </CardHeader>
-          <CardContent>
-            <img src="https://placehold.co/600x400/EEE/31343C" className="w-full h-full mb-4" alt="" />
-            <Button>Read more...</Button>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
-  );
+  return <>home</>;
 }
  
